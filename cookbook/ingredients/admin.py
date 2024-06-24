07dda82c5885b5b27b585987b8c38ed9bc351ba4
kuroyasu <xjr1300.04@gmail.com>
2024-06-24 15:15:31 +0900

    Creating GraphQL and GraphiQL views.

--- cookbook/ingredients/admin.py
+++ cookbook/ingredients/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from cookbook.ingredients.models import Category, Ingredient
+from ingredients.models import Category, Ingredient
 
 admin.site.register(Category)
 admin.site.register(Ingredient)
